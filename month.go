999a3d4033c96dceb9e7340ecf014f0992900e8a
Oleksandr Tkachuk <tksasha.me@gmail.com>
2025-02-19 14:59:25 +0200

    refactored

--- month.go
+++ month.go
@@ -7,44 +7,52 @@ import (
 )
 
 type Month struct {
-	Begin driver.Value
-	End   driver.Value
+	Number int
+	Name   string
 }
 
-func New(year, month string) Month {
+func All() []Month {
+	months := []Month{}
+
+	names := []string{
+		"Січень",
+		"Лютий",
+		"Березень",
+		"Квітень",
+		"Травень",
+		"Червень",
+		"Липень",
+		"Серпень",
+		"Вересень",
+		"Жовтень",
+		"Листопад",
+		"Грудень",
+	}
+
+	for n, name := range names {
+		months = append(months, Month{Number: n + 1, Name: name})
+	}
+
+	return months
+}
+
+func Begin(year, month string) driver.Value {
+	date := date(year, month)
+
+	return date.AddDate(0, 0, -date.Day()+1).Format(time.DateOnly)
+}
+
+func End(year, month string) driver.Value {
+	date := date(year, month)
+
+	return date.AddDate(0, 1, -date.Day()).Format(time.DateOnly)
+}
+
+func date(year, month string) time.Time {
 	date, err := time.Parse(time.DateOnly, fmt.Sprintf("%04s-%02s-01", year, month))
 	if err != nil {
 		date = time.Now()
 	}
 
-	return Month{
-		Begin: begin(date),
-		End:   end(date),
-	}
-}
-
-func begin(date time.Time) driver.Value {
-	return time.Date(
-		date.Year(),
-		date.Month(),
-		1,
-		0,
-		0,
-		0,
-		0,
-		date.Location(),
-	).Format(time.DateOnly)
-}
-
-func end(date time.Time) driver.Value {
-	return time.Date(
-		date.Year(),
-		date.Month()+1,
-		1,
-		0,
-		0,
-		0,
-		0,
-		date.Location(),
-	).Add(-1 * time.Nanosecond).Format(time.DateOnly)
+	return date
 }
--- month_test.go
+++ month_test.go
@@ -1,40 +1,76 @@
 package month_test
 
 import (
+	"slices"
 	"testing"
-	"time"
 
 	"github.com/tksasha/month"
 	"gotest.tools/v3/assert"
 )
 
-func TestNew(t *testing.T) {
-	t.Run("returns parsed date when input is valid", func(t *testing.T) {
-		month := month.New("2024", "2")
+func TestAll(t *testing.T) {
+	expected := []month.Month{
+		{Number: 1, Name: "Січень"},
+		{Number: 2, Name: "Лютий"},
+		{Number: 3, Name: "Березень"},
+		{Number: 4, Name: "Квітень"},
+		{Number: 5, Name: "Травень"},
+		{Number: 6, Name: "Червень"},
+		{Number: 7, Name: "Липень"},
+		{Number: 8, Name: "Серпень"},
+		{Number: 9, Name: "Вересень"},
+		{Number: 10, Name: "Жовтень"},
+		{Number: 11, Name: "Листопад"},
+		{Number: 12, Name: "Грудень"},
+	}
 
-		assert.Equal(t, month.Begin, "2024-02-01")
-		assert.Equal(t, month.End, "2024-02-29")
-	})
+	actual := month.All()
 
-	t.Run("returns current date when input is invalid", func(t *testing.T) {
-		month := month.New("abc", "2")
+	assert.Assert(t, slices.Equal(actual, expected))
+}
 
-		today := time.Now()
+func TestBegin(t *testing.T) {
+	testmap := map[string]string{
+		"1":  "2024-01-01",
+		"2":  "2024-02-01",
+		"3":  "2024-03-01",
+		"4":  "2024-04-01",
+		"5":  "2024-05-01",
+		"6":  "2024-06-01",
+		"7":  "2024-07-01",
+		"8":  "2024-08-01",
+		"9":  "2024-09-01",
+		"10": "2024-10-01",
+		"11": "2024-11-01",
+		"12": "2024-12-01",
+	}
 
-		begin := time.Date(today.Year(), today.Month(), 1, 0, 0, 0, 0, today.Location()).Format(time.DateOnly)
+	for number, expected := range testmap {
+		actual := month.Begin("2024", number)
 
-		end := time.Date(
-			today.Year(),
-			today.Month()+1,
-			1,
-			0,
-			0,
-			0,
-			0,
-			today.Location(),
-		).Add(-1 * time.Nanosecond).Format(time.DateOnly)
+		assert.Equal(t, actual, expected)
+	}
+}
 
-		assert.Equal(t, month.Begin, begin)
-		assert.Equal(t, month.End, end)
-	})
+func TestEnd(t *testing.T) {
+	testmap := map[string]string{
+		"1":  "2024-01-31",
+		"2":  "2024-02-29",
+		"3":  "2024-03-31",
+		"4":  "2024-04-30",
+		"5":  "2024-05-31",
+		"6":  "2024-06-30",
+		"7":  "2024-07-31",
+		"8":  "2024-08-31",
+		"9":  "2024-09-30",
+		"10": "2024-10-31",
+		"11": "2024-11-30",
+		"12": "2024-12-31",
+	}
+
+	for number, expected := range testmap {
+		actual := month.End("2024", number)
+
+		assert.Equal(t, actual, expected)
+	}
 }
